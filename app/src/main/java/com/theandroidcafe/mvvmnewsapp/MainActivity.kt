96ac72af051d649f9095fbea98965baf2b39ca4d
Vijay Mishra <vijaymshr5@gmail.com>
2021-05-17 02:21:31 +0530

    new branch created for part 2.
    Signed-off-by: Vijay Mishra <vijaymshr5@gmail.com>

--- app/src/main/java/com/theandroidcafe/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/theandroidcafe/mvvmnewsapp/MainActivity.kt
@@ -9,6 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "This is a toast message", Toast.LENGTH_SHORT).show()
     }
 }
