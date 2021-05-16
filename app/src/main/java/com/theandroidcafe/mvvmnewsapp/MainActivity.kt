cbd3bc53c23b0ec9b325697433c687b918602747
Vijay Mishra <vijaymshr5@gmail.com>
2021-05-17 01:59:27 +0530

    Added Toast
    Signed-off-by: Vijay Mishra <vijaymshr5@gmail.com>

--- app/src/main/java/com/theandroidcafe/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/theandroidcafe/mvvmnewsapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.theandroidcafe.mvvmnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "This is a toast message", Toast.LENGTH_SHORT).show()
     }
 }
